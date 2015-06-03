d065f4f77f5de8f7ff6f68edfb9a94b927dafa48
Victor Cuervo <vcuervo@gmail.com>
2015-06-03 18:21:02 +0200

    Área de un triángulo en Java

---
+++ src/com/lineadecodigo/java/math/AreaTriangulo.java
@@ -0,0 +1,25 @@
+package com.lineadecodigo.java.math;
+
+/**
+ * @file AreaTriangulo.java
+ * @version 1.0
+ * @author Víctor Cuervo. (http://lineadecodigo.com)
+ * @date   03/junio/2015
+ * @url    http://lineadecodigo.com/java/area-de-un-triangulo-con-java/
+ * @description Código para calcular el área de un triángulo.
+ */
+
+public class AreaTriangulo {
+
+	public static void main(String[] args) {
+		
+		float area_triangulo = 0;
+		int base = 5;
+		int altura = 3;
+		area_triangulo =(float)base*altura/2;
+		
+		System.out.println(area_triangulo);
+
+	}
+
+}
