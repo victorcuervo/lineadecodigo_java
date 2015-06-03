69a4cde198abde93b8a2ca2cb6577834b27d1726
Victor Cuervo <vcuervo@gmail.com>
2015-06-03 18:38:38 +0200

    Hora actual en milisegundos.

---
+++ src/com/lineadecodigo/java/basico/HoraEnMilisegundos.java
@@ -0,0 +1,20 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file HoraEnMilisegundos.java
+ * @version 1.0
+ * @author Víctor Cuervo. (http://lineadecodigo.com)
+ * @date   03/junio/2015
+ * @url    http://lineadecodigo.com/java/hora-en-milisegundos-con-java/
+ * @description Código que muestra la hora actual en milisegundos.
+ */
+
+public class HoraEnMilisegundos {
+
+	public static void main(String[] args) {
+
+		System.out.println("La hora actual en milisegundos es: " + System.currentTimeMillis());
+		
+	}
+
+}
