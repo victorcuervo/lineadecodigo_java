753ad36bc265324f99b5390a848d22f5985c94ee
Victor Cuervo <vcuervo@gmail.com>
2015-06-03 19:47:27 +0200

    Contar palabras en Java

---
+++ src/com/lineadecodigo/java/string/ContarPalabras.java
@@ -0,0 +1,28 @@
+package com.lineadecodigo.java.string;
+
+/* 
+* @file ContarPalabras.java
+* @version 1.0
+* @author Víctor Cuervo. (http://lineadecodigo.com)
+* @date   03/junio/2015
+* @url    http://lineadecodigo.com/java/contar-palabras-en-java/
+* @description Código que cuenta las palabras que hay en una frase
+*/
+
+import java.util.StringTokenizer;
+
+public class ContarPalabras {
+	
+  public static void main(java.lang.String[] args) {
+
+	    // Cadena de texto
+	    String sTexto = "En un lugar de la mancha";
+
+	    // Particionador
+	    StringTokenizer stTexto = new StringTokenizer(sTexto);
+
+	    //Volcamos el número de palabras separadas
+	    System.out.println ("El numero de palabras de la frase \"" + sTexto + "\" son " +  stTexto.countTokens() );
+  }
+
+}
