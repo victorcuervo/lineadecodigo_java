7613d5b50ad7fc52d9cadd1c5219028840ff16c0
Victor Cuervo <vcuervo@gmail.com>
2015-06-03 19:36:23 +0200

    Código que lista los elementos de una Lista

---
+++ src/com/lineadecodigo/java/util/collections/ListarLista.java
@@ -0,0 +1,41 @@
+package com.lineadecodigo.java.util.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+
+/* 
+* @file ListarLista.java
+* @version 1.1
+* @author Víctor Cuervo. (http://lineadecodigo.com)
+* @date   24/junio/2004
+* @url    http://lineadecodigo.com/java/numero-de-elementos-de-una-lista-en-java/
+* @description Código que recorre los elementos de una lista
+*/
+
+
+public class ListarLista {
+
+	public ListarLista() {
+	       super();
+	     }
+
+	  public static void main(java.lang.String[] args) {
+
+	    // Definimos una ArrayList
+	    List<String> list = new ArrayList<String>();
+	   
+	    // Añadimos elementos
+	    list.add("Andrea");
+	    list.add("Amaya");
+	    list.add("Julio");
+	   
+	    // Obtenemos un Iterador y recorremos la lista.
+	    Iterator<String> iter = list.iterator();
+	    while (iter.hasNext())
+	      System.out.println(iter.next());
+	   
+	  }
+
+}
