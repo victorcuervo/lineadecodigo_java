7202d6871b2031447a827be0a6e3709c3acce41c
Victor Cuervo <vcuervo@gmail.com>
2015-06-03 18:50:34 +0200

    Números aleatorios con Random

---
+++ src/com/lineadecodigo/java/util/NumeroRandom.java
@@ -0,0 +1,26 @@
+package com.lineadecodigo.java.util;
+
+/**
+ * @file NumeroRandom.java
+ * @version 1.0
+ * @author Víctor Cuervo. (http://lineadecodigo.com)
+ * @date   03/junio/2015
+ * @url    http://lineadecodigo.com/java/generar-un-numero-aleatorio/
+ * @description Uso de la clase Random para generar números aleatorios.
+ */
+
+import java.util.Random;
+
+public class NumeroRandom {
+
+	public static void main(String[] args) {
+		
+		Random r = new Random();
+		int limite=10;
+		
+		System.out.println(r.nextInt());
+		System.out.println(r.nextInt(limite+1));
+
+	}
+
+}
