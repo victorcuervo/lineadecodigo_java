7fcf0d38fe496441a3d002627bb44dfda013d6eb
Victor Cuervo <vcuervo@gmail.com>
2015-06-05 16:26:59 +0200

    Validar si un carácter es un dígito.

---
+++ src/com/lineadecodigo/java/basico/EsUnDigito.java
@@ -0,0 +1,25 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file EsUnDigito.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   30/diciembre/2006
+ * @url  http://lineadecodigo.com/java/¿es-ese-caracter-un-numero/
+ * @description Valida si una cadena introducida por pantalla es un número
+ */
+
+public class EsUnDigito {
+
+	public static void main(String[] args) {
+		
+		
+		if (Character.isDigit('3'))
+		    System.out.println("El caracter es un dígito");
+		else
+		    System.out.println("El caracter NO es un dígito");
+		
+
+	}
+
+}
