a9e0309d4eead09a15f77288e4fe11cb8d0c1f82
Victor Cuervo <vcuervo@gmail.com>
2015-06-05 12:44:28 +0200

    Lista de números ascendentes en Java.

---
+++ src/com/lineadecodigo/java/basico/NumerosAscendentes.java
@@ -0,0 +1,21 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file NumerosAscendentes.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   14/enero/2007
+ * @url    http://lineadecodigo.com/java/numeros-ascendentes-en-java/
+ * @description Uso de un bucle para conseguir una lista de números ascendentes.  
+ */
+
+public class NumerosAscendentes {
+
+	public static void main(String[] args) {
+
+		for (int x=1;x<=100;x++){
+			System.out.println(x);
+		}
+	}
+	
+}
