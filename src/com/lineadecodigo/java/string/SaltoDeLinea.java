4e42034558c7c53581e36f4347737c8699af4f44
Victor Cuervo <vcuervo@gmail.com>
2015-06-05 18:07:12 +0200

    Generar una cadena que contenga un salto de línea

---
+++ src/com/lineadecodigo/java/string/SaltoDeLinea.java
@@ -0,0 +1,22 @@
+package com.lineadecodigo.java.string;
+
+/**
+ * @file SaltoDeLinea.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   20/diciembre/2006
+ * @url  http://lineadecodigo.com/java/string-con-salto-de-linea-en-java/
+ * @description Generar una cadena que tenga un salto de línea
+ */
+
+
+public class SaltoDeLinea {
+
+	public static void main(String[] args) {
+
+		String cadena = new String("Mi texto con\nun salto de línea");
+		System.out.println(cadena);
+
+	}
+
+}
