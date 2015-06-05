c4b8d3e6a3dae979b4a9cd160f29a0aca48d8620
Victor Cuervo <vcuervo@gmail.com>
2015-06-05 13:03:48 +0200

    Tabular cadenas en Java

---
+++ src/com/lineadecodigo/java/string/TabularCadena.java
@@ -0,0 +1,24 @@
+package com.lineadecodigo.java.string;
+
+/**
+ * @file TabularCadena.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   6/enero/2007
+ * @url    http://lineadecodigo.com/java/tabular-cadenas-en-java/
+ * @description Tabular cadenas de texto en Java  
+ */
+
+public class TabularCadena {
+	
+	public static void main(String[] args) {
+		
+		String cadenaSinTabular = "Mi cadena de texto. Mi segunda cadena de texto";
+		String cadenaTabulada = "Mi cadena de texto.\tMi segunda cadena de texto";
+		
+		System.out.println(cadenaSinTabular);
+		System.out.println(cadenaTabulada);
+		
+	}
+
+}
