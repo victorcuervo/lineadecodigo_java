119c5c93f67ef466bb02eecef003c151ee161976
Victor Cuervo <vcuervo@gmail.com>
2015-06-08 12:32:33 +0200

    Números Pares en Java

---
+++ src/com/lineadecodigo/java/basico/NumeroPar.java
@@ -0,0 +1,30 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file SaltoDeLinea.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   18/diciembre/2006
+ * @url  http://lineadecodigo.com/java/numeros-pares-en-java/
+ * @description Código que nos ayuda a saber si un número es par con Java
+ */
+
+public class NumeroPar {
+	
+	static boolean esPar(int numero){
+	    if (numero%2==0) return true; else return false;
+	}
+
+	public static void main(String[] args) {
+		
+		if (esPar(4) )
+		    System.out.println("El numero es par");
+		else
+		    System.out.println("El número es impar");
+		
+		if (!esPar(5))
+		    System.out.println("El número es impar");
+
+	}
+
+}
