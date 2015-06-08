df67dba8fc8a101c42e477e6290f1894c1ad9a6d
Victor Cuervo <vcuervo@gmail.com>
2015-06-08 13:23:37 +0200

    Tamaño de una cadena con Java

---
+++ src/com/lineadecodigo/java/string/TamanioCadena.java
@@ -0,0 +1,23 @@
+package com.lineadecodigo.java.string;
+
+/**
+ * @file TamanioCadena.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   13/diciembre/2006
+ * @url  http://lineadecodigo.com/java/tamano-de-una-cadena-en-java/
+ * @description Código que nos calcula el tamaño de una cadena en Java
+ */
+
+
+public class TamanioCadena {
+
+	public static void main(String[] args) {
+		
+		String sMiCadena = "En un lugar de la Mancha";
+		System.out.println("Cadena de Texto: '" + sMiCadena + "'");
+		System.out.println("Tamaño de la cadena: " +   sMiCadena.length() + " caracteres");
+
+	}
+
+}
