75d44637d9b5e4e16037fa14cf446ba2e257c446
Victor Cuervo <vcuervo@gmail.com>
2015-06-08 12:44:49 +0200

    Tipos de Comentarios en Java

---
+++ src/com/lineadecodigo/java/basico/ComentariosEnJava.java
@@ -0,0 +1,29 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file ComentariosEnJava.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   17/diciembre/2006
+ * @url  http://lineadecodigo.com/java/comentarios-en-java/
+ * @description Clase Java con los diferentes tipos de comentarios que se pueden utilizar.
+ */
+
+
+public class ComentariosEnJava {
+
+	public static void main(String[] args) {
+		
+		// Comentario de una línea
+		
+		/* Comentario
+		de varias
+		líneas */
+		
+		/** Comentario para JavaDoc */
+		
+		System.out.println("Ejemplo de Comentarios en Java");
+
+	}
+
+}
