02f23ec1d3668404d9e5ef6f8624d2b472ca9813
victorcuervo <vcuervo@gmail.com>
2015-06-15 01:27:23 +0200

    Cargar números aleatorios en un array.

---
+++ src/com/lineadecodigo/java/basico/arrays/CargarArrayNumerosAleatorios.java
@@ -0,0 +1,28 @@
+package com.lineadecodigo.java.basico.arrays;
+
+/**
+ * @file CargarArrayNumerosAleatorios.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   15/junio/2015
+ * @url    http://lineadecodigo.com/java/llenar-un-array-con-numeros-aleatorios-en-java/
+ * @description Cargar un array con nœmeros aleatorios
+ */
+
+public class CargarArrayNumerosAleatorios {
+
+	public static void main(String[] args) {
+		
+		int[] numeros = new int[10];
+		
+		// Cargamos el array con los nœmeros aleatorios
+		for (int x=0;x<numeros.length;x++)
+			numeros[x] = (int) (Math.random()*20)+1;
+		
+		// Listar el array
+		for (int x=0;x<numeros.length;x++)
+			System.out.println(numeros[x]);		
+
+	}
+
+}
