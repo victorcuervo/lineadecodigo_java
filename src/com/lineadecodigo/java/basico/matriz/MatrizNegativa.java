ff7bf6e2531d80bcc84dba1954719954a09fe5b5
victorcuervo <vcuervo@gmail.com>
2015-08-02 20:24:26 +0200

    Matriz Negativa con java

---
+++ src/com/lineadecodigo/java/basico/matriz/MatrizNegativa.java
@@ -0,0 +1,32 @@
+package com.lineadecodigo.java.basico.matriz;
+
+/**
+ * @file MatrizNegativa.java
+ * @version 1.0
+ * @author Línea de Codigo (http://lineadecodigo.com)
+ * @date   1/agosto/2015
+ * @url    http://lineadecodigo.com/java/matriz-negativa-con-java/
+ * @description Código que genera una matriz negativa cambiado en signo a todos los elementos de la matriz.
+ */
+
+public class MatrizNegativa {
+
+	public static void main(String[] args) {
+		
+		int[][] m = {{0,-2,4},{2,0,2},{-4,-2,0}};
+		
+		System.out.println("Matriz Inicial");
+		Matriz.imprimir(m);
+		
+		for (int x=0; x < m.length; x++) {
+            for (int y=0; y < m[x].length; y++) {
+            		m[x][y] = -m[x][y];
+            }
+		}
+
+		System.out.println("Matriz Negativa");
+		Matriz.imprimir(m);
+		
+	}
+
+}
