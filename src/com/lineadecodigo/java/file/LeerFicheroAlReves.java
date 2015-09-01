0b65b532e4910c740755a5a18e18334a39ab44a8
Victor Cuervo <vcuervo@gmail.com>
2015-09-01 16:55:22 +0200

    Leer fichero de forma inversa

---
+++ src/com/lineadecodigo/java/file/LeerFicheroAlReves.java
@@ -0,0 +1,53 @@
+package com.lineadecodigo.java.file;
+
+/**
+ * @file LeerFicheroAlReves.java
+ * @version 1.1
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   01/sep/2015
+ * @url    http://lineadecodigo.com/java/leer-fichero-al-reves-con-java/
+ * @descriptio Código que realiza la lectura de un fichero y lo muestra de forma inversa.
+ */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LeerFicheroAlReves {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			// Lista para almacenar lo leido
+			LinkedList<String> list = new LinkedList<String>();
+			
+			FileReader fr = new FileReader("resources/fichero_lineas.txt");
+			BufferedReader bf = new BufferedReader(fr);
+			String sCadena;
+			while ((sCadena = bf.readLine())!=null) {
+				list.add(sCadena);				
+			}		
+			
+			// Cerramos el Buffer
+			bf.close();
+			
+			Iterator<String> it = list.descendingIterator();
+			 while(it.hasNext())
+			    	System.out.println(it.next());
+				
+			 
+		} catch (FileNotFoundException fnfe){
+			fnfe.printStackTrace();
+		} catch (IOException ioe){
+			ioe.printStackTrace();
+		}
+
+		
+		
+	}
+
+}
