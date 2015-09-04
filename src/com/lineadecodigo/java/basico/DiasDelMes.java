70d3b4012e9296dc6be5e01db1d121b3218da1d5
Victor Cuervo <vcuervo@gmail.com>
2015-09-04 12:43:35 +0200

    Días del mes con Java

---
+++ src/com/lineadecodigo/java/basico/DiasDelMes.java
@@ -0,0 +1,39 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file DiasDelMes.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   03/sep/2015
+ * @url     http://lineadecodigo.com/java/dias-del-mes-en-java/
+ * @description Ejemplo de agrupaciones de sentencias case para poder calcular los días del mes con Java.  
+ */
+
+public class DiasDelMes {
+	
+public static void main(String[] args) {
+		
+		
+		int iMes = 3;
+		String sDias;
+
+		switch (iMes) {
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+				sDias = "El mes tiene 31 días";
+				break;
+			case 4: case 6: case 9: case 11:
+				sDias = "El mes tiene 30 días";
+				break;
+			case 2:
+				
+				sDias = "El mes tiene 28 días (o 29 días si es año bisiesto)";
+				break;			
+			default:
+				sDias = "Mes incorrecto";
+		}
+		
+		System.out.println(sDias);
+		
+	}
+
+}
