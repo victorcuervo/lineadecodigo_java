e0fbef097ab75a858f196650464ff235936c5469
Victor Cuervo <vcuervo@gmail.com>
2015-09-08 13:20:09 +0200

    Parámetros variables en Java

---
+++ src/com/lineadecodigo/java/basico/ParametrosVariables.java
@@ -0,0 +1,30 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file ParametrosVariables.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   08/sep/2015
+ * @url  http://lineadecodigo.com/java/parametros-variables-en-java/
+ * @description Implementar un método con parámetros variables
+ */
+
+
+
+public class ParametrosVariables {
+	
+	public static int sumar(int... numeros) {
+		int suma = 0;
+		for (int x=0;x<numeros.length;x++)
+			suma += numeros[x];
+		return suma;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(sumar(2,3,4,5));
+		System.out.println(sumar(2,2));
+
+	}
+
+}
