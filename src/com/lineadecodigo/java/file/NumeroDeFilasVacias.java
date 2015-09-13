61050d4a5e74c29cde6c562d6a569cbcf0816624
victorcuervo <vcuervo@gmail.com>
2015-09-13 12:56:00 +0200

    Número de líneas vacías de un fichero.

---
+++ src/com/lineadecodigo/java/file/NumeroDeFilasVacias.java
@@ -0,0 +1,50 @@
+package com.lineadecodigo.java.file;
+
+/**
+ * @file NumeroDeFilasVacias.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   13/sep/2015
+ * @url    http://lineadecodigo.com/java/numero-de-lineas-vacias-de-un-fichero/
+ * @description Código que recorre un fichero y lee sus filas para contablizar las que están vacías.
+ */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class NumeroDeFilasVacias {
+
+	public static void main(String[] args) {
+		
+		
+		
+		try {
+			FileReader fr = new FileReader("resources/fichero.txt");
+		
+			BufferedReader bf = new BufferedReader(fr);
+			
+			String sCadena = "";
+			long lNumeroLineasVacias = 0;
+			
+			while ((sCadena = bf.readLine())!=null) {
+				if (sCadena.isEmpty())
+					lNumeroLineasVacias++;
+			}
+			
+			System.out.println("El fichero tiene " + lNumeroLineasVacias + " lineas vacías");
+			
+			bf.close();
+			
+		} catch (FileNotFoundException fnfe){
+			  fnfe.printStackTrace();
+		} catch (IOException ioe){
+		  ioe.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
