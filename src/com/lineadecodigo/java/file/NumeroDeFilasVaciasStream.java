af83127c4905966863ebcb0e45fa7c478341bd64
victorcuervo <vcuervo@gmail.com>
2015-09-19 20:29:23 +0200

    Número de líneas vacías mediante programaión funcional.

---
+++ src/com/lineadecodigo/java/file/NumeroDeFilasVaciasStream.java
@@ -0,0 +1,60 @@
+package com.lineadecodigo.java.file;
+
+/**
+ * @file NumeroDeFilasVaciasStream.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   19/sep/2015
+ * @url    http://lineadecodigo.com/java/lineas-vacias-de-un-fichero-con-un-stream/
+ * @description Calcular el número de filas que hay en un fichero mediante programación funcional y un Stream.
+ */
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class NumeroDeFilasVaciasStream {
+
+	public static void main(String[] args) {
+		
+		long lNumeroLineasVacias = 0;
+		
+		try {
+			FileReader fr = new FileReader("resources/fichero.txt");
+		
+			BufferedReader bf = new BufferedReader(fr);
+			
+			long vacias = bf.lines().filter(l -> l.isEmpty()).count();
+			
+			
+			
+			System.out.println(vacias);
+			
+			/*
+			
+			String sCadena = "";
+			
+			
+			
+			
+			while ((sCadena = bf.readLine())!=null) {
+				if (sCadena.isEmpty())
+					lNumeroLineasVacias++;
+			}
+			
+			*/
+			
+			bf.close();
+			
+		} catch (FileNotFoundException fnfe){
+			  fnfe.printStackTrace();
+		} catch (IOException ioe){
+		  ioe.printStackTrace();
+		}
+		
+		System.out.println("El fichero tiene " + lNumeroLineasVacias + " lineas vacías");
+		
+	}
+
+}
