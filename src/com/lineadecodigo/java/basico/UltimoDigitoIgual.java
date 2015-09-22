1c86fd15eee046908269a97892866a7c605ca4d6
victorcuervo <vcuervo@gmail.com>
2015-09-23 00:47:00 +0200

    Último dígito igual de dos números en Java.

---
+++ src/com/lineadecodigo/java/basico/UltimoDigitoIgual.java
@@ -0,0 +1,31 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file UltimoDigitoIgual.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   21/sep/2015
+ * @url    http://lineadecodigo.com/java/ultimo-digito-igual/
+ * @description Comprobar si dados dos números estos tienen el último dígito igual.
+ */
+
+
+public class UltimoDigitoIgual {
+	
+	public static void main(String[] args) {
+		
+		int in1 = 1543;
+		int in2 = 13;
+		
+		String n1 = Integer.toString(in1);
+		String n2 = Integer.toString(in2);
+		
+		if (n1.charAt(n1.length()-1) == n2.charAt(n2.length()-1))			
+			System.out.println("Acaban en el mismo dígito");
+		else
+			System.out.println("El último dígito es diferente");
+			
+		
+	}
+
+}
