759a238ca9884101fd7827e23a17851abd8054d5
victorcuervo <vcuervo@gmail.com>
2015-09-22 23:53:20 +0200

    Cierre de un programa Java.

---
+++ src/com/lineadecodigo/java/hilos/CierreAplicacion.java
@@ -0,0 +1,38 @@
+package com.lineadecodigo.java.hilos;
+
+/**
+ * @file CierreAplicacion.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   21/sep/2015
+ * @url    http://lineadecodigo.com/java/finalizar-un-programa-java/
+ * @description Controlar la finalización de un programa Java
+ */
+
+public class CierreAplicacion {
+
+	public void attachShutDownHook(){
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+	
+			@Override
+			public void run() {
+				System.out.println("Esto lo ejecutamos cuando acabe el programa");
+			}
+		});
+		
+		System.out.println("Creado el hook en el programa");
+		
+	 }
+	
+	
+	public static void main(String[] args) {
+	
+		CierreAplicacion app = new CierreAplicacion();
+		app.attachShutDownHook();
+		System.out.println("Última instrucción del programa");
+		System.exit(0);
+		
+	}
+
+}
