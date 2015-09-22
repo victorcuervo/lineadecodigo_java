6ae1e637c6b685cc4e582e914afa5738dc475225
victorcuervo <vcuervo@gmail.com>
2015-09-23 01:17:10 +0200

    Comprobar que el último dígito de un número es igual sin usar cadenas y apoyándonos en operaciones matemáticas.

---
+++ src/com/lineadecodigo/java/basico/UltimoDigitoIgualSinString.java
@@ -0,0 +1,31 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file UltimoDigitoIgualSinString.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   21/sep/2015
+ * @url    http://lineadecodigo.com/java/ultimo-digito-igual-con-operaciones-matematicas/
+ * @description Comprobar si dados dos números estos tienen el último dígito igual mediante operaciones matemáticas.
+ */
+
+
+public class UltimoDigitoIgualSinString {
+	
+	public static void main(String[] args) {
+		
+		int in1 = 12348;
+		int in2 = 138;
+		
+		int digito1 = in1%10;
+		int digito2 = in2%10;		
+				
+		if (digito1 == digito2)			
+			System.out.println("Acaban en el mismo dígito");
+		else
+			System.out.println("El último dígito es diferente");
+		
+		
+	}
+
+}
