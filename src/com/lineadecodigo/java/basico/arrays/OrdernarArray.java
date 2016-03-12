a59a6f7f3ea7b7264e78ff69361837baf2522812
victorcuervo <vcuervo@gmail.com>
2016-03-12 20:00:01 +0100

    Ordenar un Array en Java

---
+++ src/com/lineadecodigo/java/basico/arrays/OrdernarArray.java
@@ -0,0 +1,37 @@
+package com.lineadecodigo.java.basico.arrays;
+
+import java.util.Arrays;
+
+/**
+ * @file RotarArray.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   12/marzo/2016
+ * @url    http://lineadecodigo.com/java/ordenar-array-java/
+ * @description Código que ordena un array
+ */
+
+public class OrdernarArray {
+	
+	public static void main(String[] args) {
+		
+		int[] numeros = {7,4,5,2,1,10,8};
+
+		imprimir(numeros);
+		Arrays.sort(numeros);	
+		imprimir(numeros);
+		
+
+	}
+	
+	
+	public static void imprimir(int[] numeros){
+		
+		for (int numero:numeros)
+			System.out.println(numero);
+		
+		System.out.println("-----");
+		
+	}
+
+}
