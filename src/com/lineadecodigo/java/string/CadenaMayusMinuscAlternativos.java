a61e784ee20ccb821927382b4c1dc3f4c113e2f0
victorcuervo <vcuervo@gmail.com>
2016-03-12 20:37:11 +0100

    Convertir caracteres de una cadena a mayúsculas y minúsculas alternativamente.

---
+++ src/com/lineadecodigo/java/string/CadenaMayusMinuscAlternativos.java
@@ -0,0 +1,33 @@
+package com.lineadecodigo.java.string;
+
+/**
+ * @file CadenaMayusMinuscAlternativos.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   12/marzo/2016
+ * @url  http://lineadecodigo.com/java/palabra-mayusculas-minusculas/
+ * @description Convierte los caracteres de una cadena a mayúsculas y minúsculas alternativamente
+ */
+
+public class CadenaMayusMinuscAlternativos {
+
+	public static void main(String[] args) {
+		
+		StringBuilder sCadena = new StringBuilder("Esto es una frase muy interesante");
+		String caracter;
+		System.out.println(sCadena);
+		
+		for (int x=0;x<sCadena.length();x++) {
+			caracter = Character.toString(sCadena.charAt(x));
+			if (x%2 == 0)
+				sCadena.setCharAt(x,caracter.toUpperCase().charAt(0));
+			else
+				sCadena.setCharAt(x,caracter.toLowerCase().charAt(0));
+				
+		}
+		
+		System.out.println(sCadena);
+
+	}
+
+}
