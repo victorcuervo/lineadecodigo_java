e826798c4ac48e9347f63a2d4e4944c091fb48fb
victorcuervo <vcuervo@gmail.com>
2016-03-13 12:03:05 +0100

    Manejo de una cadena como si fuese un Stream

---
+++ src/com/lineadecodigo/java/util/stream/CadenaComoStream.java
@@ -0,0 +1,36 @@
+package com.lineadecodigo.java.util.stream;
+
+/**
+ * @file CadenaComoStream.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   13/marzo/2016
+ * @url    http://lineadecodigo.com/java/string-stream-java/
+ * @description Manipular una cadena como si fuese un Stream 
+ */
+
+public class CadenaComoStream {
+
+	public static void main(String[] args) {
+		
+		
+		String sCadena = "Esto es una cadena de texto";
+
+	    sCadena.chars()
+	    	.forEach(c -> System.out.print(Character.toString((char) c)));
+	    
+	    System.out.println();
+	    
+	    sCadena.chars()
+	    	.parallel()
+	    	.forEach(c -> System.out.print(Character.toString((char) c)));
+
+	    System.out.println();
+	    
+	    sCadena.chars()
+    	.filter(c -> c!='a')
+    	.forEach(c -> System.out.print(Character.toString((char) c)));
+
+	}
+
+}
