d22e521a4d4b8079eabe762855fa90d077523f9a
victorcuervo <vcuervo@gmail.com>
2016-04-04 00:58:50 +0200

    Extraer enlaces de una web con Java

---
+++ src/com/lineadecodigo/java/external/jaunt/ExtraerEnlaces.java
@@ -0,0 +1,38 @@
+package com.lineadecodigo.java.external.jaunt;
+
+/**
+ * @file ExtraerEnlaces.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date   4/abril/2016
+ * @url    http://lineadecodigo.com/java/enlaces-una-web-java/
+ * @description Uso de la librería Jaunt para extraer los enlaces de una web
+ */
+
+import com.jaunt.Elements;
+import com.jaunt.JauntException;
+import com.jaunt.ResponseException;
+import com.jaunt.UserAgent;
+import com.jaunt.Element;
+
+public class ExtraerEnlaces {
+
+	public static void main(String[] args) {
+		
+		try {
+			UserAgent userAgent = new UserAgent();
+			userAgent.visit("http://manualweb.net");			
+			
+			Elements links = userAgent.doc.findEvery("<a>");
+			for(Element link : links) System.out.println(link.getAt("href"));
+			
+			
+		} catch (ResponseException re) {
+			re.printStackTrace();
+		} catch (JauntException j){
+			j.printStackTrace();
+		}
+			
+	}
+
+}
