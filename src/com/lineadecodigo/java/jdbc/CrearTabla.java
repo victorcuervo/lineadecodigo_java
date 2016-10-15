50db51045976b4da39956481ddd394057deaa937
victorcuervo <vcuervo@gmail.com>
2016-10-15 19:12:44 +0200

    Crear Tabla con JDBC

---
+++ src/com/lineadecodigo/java/jdbc/CrearTabla.java
@@ -0,0 +1,57 @@
+package com.lineadecodigo.java.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class CrearTabla {
+	
+	public static void main(String[] args) {
+		
+		Connection con = null;
+	    PreparedStatement stmt = null;
+	    ResultSet rs = null;
+
+	    String sDriver = "com.mysql.jdbc.Driver";
+	    String sURL = "jdbc:mysql://localhost:3306/lineadecodigo";
+	    
+	    Properties p = new Properties();
+	    p.put("user", "root");
+	    p.put("password","");
+
+	    try {
+
+	      Class.forName(sDriver).newInstance();    
+	      con = DriverManager.getConnection(sURL,p);
+	      
+	      try {
+	        	      
+	        stmt = con.prepareStatement("CREATE TABLE paises (pais varchar(50) NOT NULL, acronimo CHAR(3), poblacion INT(20))");  
+	    	stmt.execute(); 
+	    	stmt.close();
+	        	        
+	   
+	      } catch (SQLException sqle) { 
+	           System.out.println("Error en la ejecución: " 
+	             + sqle.getErrorCode() + " " + sqle.getMessage());    
+	      }
+	   
+	    } catch (Exception e) { 
+	      System.out.println("Error en la conexión: " + e.toString() );
+	    } finally {
+	      try {
+	        // Cerramos posibles conexiones abiertas
+	        if (rs!=null) rs.close();
+	        if (stmt!=null) stmt.close();
+	        if (con!=null) con.close();    
+	      } catch (Exception e) {
+	        System.out.println("Error cerrando conexiones: " + e.toString());
+	      } 
+	    } 
+
+	}
+
+}
