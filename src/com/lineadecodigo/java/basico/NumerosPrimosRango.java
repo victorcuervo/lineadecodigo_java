b01434673876fe2f2dcb3605ea85f2c825623908
Víctor Cuervo <vcuervo@gmail.com>
2017-09-17 23:23:19 +0200

    close #3 Cálculo de un número primo en Java para un rango de números.

---
+++ src/com/lineadecodigo/java/basico/NumerosPrimosRango.java
@@ -0,0 +1,37 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file NumerosPrimosRango.java
+ * @version 1.0
+ * @author Linea de Codigo (http://lineadecodigo.com)
+ * @date  17/septiembre/2017
+ * @url    http://lineadecodigo.com/java/xxx/
+ * @description Listar los números primos de un rango
+ */
+
+public class NumerosPrimosRango {
+
+	public static boolean esPrimo(int numero){
+		int contador = 2;
+		boolean primo=true;
+		while ((primo) && (contador!=numero)){
+		  if (numero % contador == 0)
+		    primo = false;
+		  contador++;
+		}
+		return primo;
+	}	
+	
+	public static void main(String[] args) {
+		
+		int iInicioRango = 50;
+		int iFinRango = 97;
+		
+		for (int x=iInicioRango;x<=iFinRango;x++) {
+			if (esPrimo(x))
+				System.out.print(x + " ");				
+		}		
+		
+	}
+
+}
