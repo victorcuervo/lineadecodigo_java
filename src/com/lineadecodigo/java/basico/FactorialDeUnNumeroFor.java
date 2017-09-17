e20d07d71bd284258b6be37c41a39044a48e7b75
Víctor Cuervo <vcuervo@gmail.com>
2017-09-17 23:09:21 +0200

    close #6 factorial de un número con for

---
+++ src/com/lineadecodigo/java/basico/FactorialDeUnNumeroFor.java
@@ -0,0 +1,30 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file FactorialDeUnNumero.java
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   17/septiembre/2017
+ * @url    http://lineadecodigo.com/java/factorial-java-bucle-for/
+ * @description Calcular un factorial mediante un bucle for
+ */
+
+public class FactorialDeUnNumeroFor {
+
+
+	public static void main(String[] args) {
+		
+		// Numero del que queremos calcular el factorial
+		int iNumero = 4;
+
+		// Variable con el valor del factorial
+		int iFactorial = 1;
+
+		
+		for (int x=2;x<=iNumero;x++)
+			iFactorial = iFactorial * x;
+		
+		System.out.println("El factorial del número " + Integer.toString(iNumero) +  " es " + Integer.toString(iFactorial));
+		
+	}	
+}
