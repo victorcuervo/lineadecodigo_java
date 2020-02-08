4cad7e2cdfd07374c8912e642246456416645da0
Víctor Cuervo <vcuervo@gmail.com>
2020-02-08 14:33:48 +0100

    Calcular los números de un dígito mediante un logaritmo en base 10.

---
+++ src/com/lineadecodigo/java/basico/NumeroDigitosLogaritmo.java
@@ -0,0 +1,21 @@
+package com.lineadecodigo.java.basico;
+
+/**
+ * @file NumeroDigitosLogaritmo.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date  08/febrero/2020
+ * @url    http://lineadecodigo.com/java/pdte/
+ * @description Número de dígitos de un número mediante el uso de un Logaritmo
+ */
+
+public class NumeroDigitosLogaritmo {
+
+	public static void main(String[] args) {			
+		int iNumero = 1234589;
+		int length = (int) (Math.log10(iNumero) + 1);
+		System.out.println("El número " + iNumero + " cuenta con " + length + " dígitos");
+		
+	}
+	
+}
