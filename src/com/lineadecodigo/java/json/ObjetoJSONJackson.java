bc15146930c945439a75bfe00a8a5c4f15472487
Víctor Cuervo <vcuervo@gmail.com>
2020-03-14 21:26:38 +0100

    Objeto a JSON mediante Jackson

---
+++ src/com/lineadecodigo/java/json/ObjetoJSONJackson.java
@@ -0,0 +1,29 @@
+package com.lineadecodigo.java.json;
+
+import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lineadecodigo.base.Persona;
+
+/**
+ * @file ObjetoJSONJackson.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   14/marzo/2020
+ * @url    http://lineadecodigo.com/java/pdte/
+ * @description Objeto a JSON mediante Jackson
+ */
+
+public class ObjetoJSONJackson {
+
+	public static void main(String[] args) throws IOException {
+
+		Persona p = new Persona("Marta",34);
+		
+		ObjectMapper mapper = new ObjectMapper();		 
+	    String jsonStr = mapper.writeValueAsString(p);
+	    
+	    System.out.println(jsonStr);	   
+		
+	}
+
+}
