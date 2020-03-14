a0a9b184343b0b90d519d3aca6040b66d4555a77
Víctor Cuervo <vcuervo@gmail.com>
2020-03-14 21:41:50 +0100

    Crear un subarray a partir de un array existente.

---
+++ src/com/lineadecodigo/java/basico/arrays/SubArray.java
@@ -0,0 +1,31 @@
+package com.lineadecodigo.java.basico.arrays;
+
+import java.util.Arrays;
+
+/**
+ * @file SubArray.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   14/marzo/2020
+ * @url    http://lineadecodigo.com/java/pdte/
+ * @description Obtener una parte de un array en un nuevo array.
+ */
+
+public class SubArray {
+	
+	public static<T> T[] subarray (T[] array, int inicio, int fin) {
+		return Arrays.copyOfRange(array,inicio-1,fin);
+	}
+
+	public static void main(String[] args) {
+
+		Integer[] numeros = {1,2,3,4,5,6,7,8,9,10};		
+		Integer[] parcial = subarray(numeros,1,4);
+		
+		for (int x=0;x<parcial.length;x++){
+			System.out.println(parcial[x]);
+		}		
+		
+	}
+
+}
