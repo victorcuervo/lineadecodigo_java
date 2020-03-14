624182c0753c0adb7a56e9d2b4567324f874dfca
Víctor Cuervo <vcuervo@gmail.com>
2020-03-14 19:30:32 +0100

    Mostrar un array como una cadena con corchetes y separadores.

---
+++ src/com/lineadecodigo/java/basico/arrays/ArraytoString.java
@@ -0,0 +1,24 @@
+package com.lineadecodigo.java.basico.arrays;
+
+import java.util.Arrays;
+
+/**
+ * @file ArrayChar2String.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   14/marzo/202s0
+ * @url    http://lineadecodigo.com/java/pdte/
+ * @description Visualizar el contenido de un array como si fuese una cadena, pero con corchetes y separaciones de elementos.s
+ */
+
+public class ArraytoString {
+
+	public static void main(String[] args) {
+
+		int numeros[] = {12,20,98,3,56,69,7,19};	   
+		System.out.println(Arrays.toString(numeros));
+		
+		
+	}
+
+}
