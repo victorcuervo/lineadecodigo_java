f4d20750da10fe8abd06ca1e91a959eb4c55e7d5
Víctor Cuervo <vcuervo@gmail.com>
2020-09-10 20:02:01 +0200

    Objeto a JSON con JSON-Simple

--- src/com/lineadecodigo/java/json/ObjetoJSON.java
+++ src/com/lineadecodigo/java/json/ObjetoJSON.java
@@ -5,7 +5,7 @@ package com.lineadecodigo.java.json;
  * @version 1.0
  * @author Víctor Cuervo (http://lineadecodigo.com)
  * @date   14/marzo/2020
- * @url    http://lineadecodigo.com/java/pdte/
+ * @url    http://lineadecodigo.com/java/convertir-un-objeto-a-json-con-json-simple/
  * @description Objeto a JSON mediante JSON-Simple
  */
 
@@ -13,8 +13,7 @@ import com.github.cliftonlabs.json_simple.Jsoner;
 
 public class ObjetoJSON {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {		
 
 		Alumno alumno = new Alumno("Mari Cielo",22);
 		System.out.println(Jsoner.serialize(alumno));
