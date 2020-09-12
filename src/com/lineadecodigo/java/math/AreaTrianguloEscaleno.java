5c59d52030cc1b69a6c6d82b8a701caebe374674
Víctor Cuervo <vcuervo@gmail.com>
2020-09-12 13:23:24 +0200

    Área triángulo escaleno

---
+++ src/com/lineadecodigo/java/math/AreaTrianguloEscaleno.java
@@ -0,0 +1,30 @@
+package com.lineadecodigo.java.math;
+
+/**
+ * @file AreaTrianguloEscaleno.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   12/septiembre/2020
+ * @url    http://lineadecodigo.com/java/pdte/
+ * @description Uso de la formula de Heron para el cálculo del área de un triángulo escaleno 
+ */
+
+
+public class AreaTrianguloEscaleno {
+    
+    public static void main(String[] args) {
+        
+        // Longitud de los lados
+        double lado1 = 3;
+        double lado2 = 2;
+        double lado3 = 4;
+
+        double semiperimetro = (lado1+lado2+lado3)/2;
+        System.out.println("El semiperímetro es de " + semiperimetro);
+
+        double area = Math.sqrt(semiperimetro*(semiperimetro-lado1)*(semiperimetro-lado2)*(semiperimetro-lado3));
+
+        System.out.println ("El área del triángulo escaleno es " + area);
+
+    }
+}
