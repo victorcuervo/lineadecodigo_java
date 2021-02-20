fdd4b13b55a5e793a974aea7bb6edc5101681e89
victorcuervo <vcuervo@gmail.com>
2021-02-20 19:56:59 +0100

    Publicado artículo.

--- src/com/lineadecodigo/java/basico/arrays/NumerosMismaPosicion.java
+++ src/com/lineadecodigo/java/basico/arrays/NumerosMismaPosicion.java
@@ -5,7 +5,7 @@ package com.lineadecodigo.java.basico.arrays;
  * @version 1.0
  * @author Víctor Cuervo (http://lineadecodigo.com)
  * @date   6/febrero/2021
- * @url    http://lineadecodigo.com/java/pdte/
+ * @url    http://lineadecodigo.com/java/numeros-en-la-misma-posicion-de-dos-arrays/
  * @description Ver que números están en la misma posición de 2 arrays
  */
 
@@ -13,10 +13,12 @@ public class NumerosMismaPosicion {
 
 	public static void main(String[] args) {
 
-		int[] a1 = {1,2,3,4,5};
-        int[] a2 = {1,1,3,1,5};
+		int[] a1 = {1,2,3,4,5,7,9};
+        int[] a2 = {1,1,3,1,5,6};
+
+        int tamanio = (a1.length<a2.length)?a1.length:a2.length;
         
-        for (int x=0;x<a1.length;x++) {
+        for (int x=0;x<tamanio;x++) {
             if (a1[x] == a2[x]) {
                 System.out.println("El número " + a1[x] + " en la posición " + x);
             }
