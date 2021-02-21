b191e237db972f9422a4a29405d58610fcb38ce1
victorcuervo <vcuervo@gmail.com>
2021-02-21 21:03:10 +0100

    Crear una base de datos en Java

---
+++ src/com/lineadecodigo/java/jdbc/CrearBaseDeDatos.java
@@ -0,0 +1,46 @@
+package com.lineadecodigo.java.jdbc;
+
+/**
+ * @file ConexionMySQL.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   12/octubre/2016
+ * @url  http://lineadecodigo.com/java/pdte/
+ * @description Crear una base de datos con Java
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CrearBaseDeDatos {
+
+	public static void main(String[] args) {
+
+		Connection con = null;
+	    
+	    String sURL = "jdbc:mysql://localhost:3306/lineadecodigo";
+
+	    try {
+
+	      con = DriverManager.getConnection(sURL,"root","victor");
+		  Statement stmt = con.createStatement();
+		  stmt.execute("create database lineadecodigo");
+		  System.out.println("Base de datos creada correctamente");
+
+	    } catch (SQLException e) { 
+	      System.out.println("Error en la conexión:" + e.toString() );
+	    } finally {
+	      try {
+	        // Cerramos posibles conexiones abiertas
+	        if (con!=null) con.close();    
+	      } catch (Exception e) {
+	        System.out.println("Error cerrando conexiones: "
+	         + e.toString());
+	      } 
+	    } 
+
+	}
+
+}
