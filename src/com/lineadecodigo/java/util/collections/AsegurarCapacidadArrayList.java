1b4422913fd33187dfee4e99eb7fb71ad63e25a9
victorcuervo <vcuervo@gmail.com>
2021-08-06 00:59:05 +0200

    Asegurar capacidad ArrayList

---
+++ src/com/lineadecodigo/java/util/collections/AsegurarCapacidadArrayList.java
@@ -0,0 +1,34 @@
+package com.lineadecodigo.java.util.collections;
+
+import java.util.ArrayList;
+
+/**
+* @file AsegurarCapacidadArrayList.java
+* @version 1.0
+* @author Víctor Cuervo (http://lineadecodigo.com)
+* @date   6/agosto/2021
+* @url    http://lineadecodigo.com/java/xxx/
+* @description Asegura la capacidad de un ArrayList
+*/
+
+public class AsegurarCapacidadArrayList {
+
+	  public static void main(java.lang.String[] args) {
+
+	    ArrayList<String> list = new ArrayList<String>();
+
+		// Aseguramos la capacidad para 3 elementos
+		list.ensureCapacity(3);
+
+		System.out.println("El tamaño del ArrayList es " + list.size());
+
+	    // Añadimos elementos
+	    list.add("Marta");
+	    list.add("Luis");
+	    list.add("Francisco");
+	   
+		System.out.println("El tamaño del ArrayList es " + list.size());
+	    
+	  }
+
+}
