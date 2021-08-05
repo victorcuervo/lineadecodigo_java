eefb6643b69975efe9860204425a7b76f5facadb
victorcuervo <vcuervo@gmail.com>
2021-08-06 00:28:54 +0200

    Clonar un arraylist

---
+++ src/com/lineadecodigo/java/util/collections/ClonarArrayList.java
@@ -0,0 +1,42 @@
+package com.lineadecodigo.java.util.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+* @file ClonarArrayList.java
+* @version 1.0
+* @author Víctor Cuervo (http://lineadecodigo.com)
+* @date   6/agosto/2021
+* @url    http://lineadecodigo.com/java/xxx/
+* @description Código que clona un ArrayList
+*/
+
+public class ClonarArrayList {
+
+	  public static void main(java.lang.String[] args) {
+
+	    // Definimos una ArrayList
+	    ArrayList<String> list = new ArrayList<String>();
+	   
+	    // Añadimos elementos
+	    list.add("Marta");
+	    list.add("Luis");
+	    list.add("Francisco");
+	   
+	    // Obtenemos un Iterador y recorremos la lista.
+	    Iterator<String> iter = list.iterator();
+	    while (iter.hasNext())
+	      System.out.println(iter.next());
+
+		ArrayList<String> list2 = (ArrayList<String>)list.clone();
+
+		// Obtenemos un Iterador y recorremos la lista.
+		iter = list2.iterator();
+		while (iter.hasNext())
+			System.out.println(iter.next());
+		
+	   
+	  }
+
+}
