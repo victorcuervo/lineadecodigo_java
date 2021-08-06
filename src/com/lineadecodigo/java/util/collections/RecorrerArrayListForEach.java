b18caec0a0e4a1f99c5ab1e59c836b68c3c73797
victorcuervo <vcuervo@gmail.com>
2021-08-06 11:23:12 +0200

    Recorrer ArrayList con Foreach

---
+++ src/com/lineadecodigo/java/util/collections/RecorrerArrayListForEach.java
@@ -0,0 +1,29 @@
+package com.lineadecodigo.java.util.collections;
+
+import java.util.ArrayList;
+
+/**
+* @file RecorrerArrayListForEach.java
+* @version 1.0
+* @author Víctor Cuervo (http://lineadecodigo.com)
+* @date   6/agosto/2021
+* @url    http://lineadecodigo.com/java/xxx/
+* @description Asegura la capacidad de un ArrayList
+*/
+
+public class RecorrerArrayListForEach {
+
+	  public static void main(java.lang.String[] args) {
+
+	    ArrayList<String> list = new ArrayList<String>();
+
+	    // Añadimos elementos
+	    list.add("Marta");
+	    list.add("Luis");
+	    list.add("Francisco");
+
+		list.forEach((n) -> System.out.println(n));
+
+	  }
+
+}
