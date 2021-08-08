5bb108bf6fe482fd87c9f6d953bb1cc25741229f
victorcuervo <vcuervo@gmail.com>
2021-08-08 10:41:05 +0200

    Añadir y eliminar elementos de una lista. Control de vacía.

---
+++ src/com/lineadecodigo/java/util/collections/AddingDeletingElementsArrayList.java
@@ -0,0 +1,53 @@
+package com.lineadecodigo.java.util.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+* @file AddingDeletingElementsArrayList.java
+* @version 1.0
+* @author Víctor Cuervo (http://lineadecodigo.com)
+* @date   8/agosto/2021
+* @url    http://lineadecodigo.com/java/xxx/
+* @description Añadir y eliminar elementos de un ArrayList. Hay que controlar que no está vacía.
+*/
+
+public class AddingDeletingElementsArrayList {
+
+	  public static void main(java.lang.String[] args) {
+
+	    // Definimos una ArrayList
+	    ArrayList<String> list = new ArrayList<String>();
+	  
+		// Lectura por consola
+		Scanner reader = new Scanner(System.in);
+		String sTexto = "";
+
+	    // Añadimos elementos
+	    list.add("Elemento");
+	    list.add("Elemento");
+	    list.add("Elemento");
+	   
+		System.out.println("Escriba ADD para añadir un elemento o DEL para borrarlo. FIN para terminar");
+		
+		do {
+			sTexto = reader.next();
+			
+			if (sTexto.equals("ADD"))
+				list.add("Elemento");
+			else if (sTexto.equals("DEL"))
+				if (!list.isEmpty())
+					list.remove(list.size()-1);
+				else
+					System.out.println("La lista está vacía. No puedes borrar más elementos.");
+			
+			System.out.println("Hay " + list.size() + " elementos en la lista");
+
+		} while(!sTexto.equals("FIN"));
+
+		reader.close();
+	   
+	  }
+
+}
