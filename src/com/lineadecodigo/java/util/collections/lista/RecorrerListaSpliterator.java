2375c6a58b994c97945da070375a9fa475cb2df0
victorcuervo <vcuervo@gmail.com>
2021-08-14 11:29:51 +0200

    Recorrer lista con Spliterator

---
+++ src/com/lineadecodigo/java/util/collections/lista/RecorrerListaSpliterator.java
@@ -0,0 +1,28 @@
+package com.lineadecodigo.java.util.collections.lista;
+
+/**
+ * @file RecorrerListaSpliterator.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   14/agosto/2021
+ * @url    http://lineadecodigo.com/java/xxx/
+ * @description Recorrer los elementos de un ArrayList mediante un spliterator.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Spliterator;
+
+public class RecorrerListaSpliterator {
+
+	public static void main(String[] args) {
+		
+		// ArrayList con tamaño
+		ArrayList<Integer> lista = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		
+		Spliterator<Integer> si = lista.spliterator();
+		si.forEachRemaining(n -> System.out.println(n));
+	
+	}
+
+}
