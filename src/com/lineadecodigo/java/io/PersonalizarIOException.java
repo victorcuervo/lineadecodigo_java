48ff8f5f059b8798f822a9d6c2db44c6ca9884d0
victorcuervo <vcuervo@gmail.com>
2021-08-18 18:24:47 +0200

    Personalizar IOException

---
+++ src/com/lineadecodigo/java/io/PersonalizarIOException.java
@@ -0,0 +1,51 @@
+package com.lineadecodigo.java.io;
+
+/**
+ * @file PersonalizarIOException.java
+ * @version 1.0
+ * @author Víctor Cuervo (http://lineadecodigo.com)
+ * @date   18/agosto/2021
+ * @url    http://lineadecodigo.com/java/xxx/
+ * @description Cómo personalizar una IOException 
+ */
+
+
+import java.io.*;
+
+public class PersonalizarIOException {
+    
+    public static long numeroLineas(String fichero) throws IOException {
+
+        long lNumeroLineas = 0;
+
+        try{
+            FileReader file = new FileReader(fichero);
+            BufferedReader bf = new BufferedReader(file);
+			
+			while ((bf.readLine())!=null) {
+			  lNumeroLineas++;
+			}
+			
+			bf.close();
+        }
+        catch(IOException ioe){
+            throw new IOException("Error en la lectura del fichero",ioe);
+        }        
+
+        return lNumeroLineas;
+    }
+
+    public static void main(String[] args) {
+
+        String fichero = "datos.txt";
+        try {
+            numeroLineas(fichero);
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
+            ioe.printStackTrace();
+        }
+
+
+    }
+
+}
