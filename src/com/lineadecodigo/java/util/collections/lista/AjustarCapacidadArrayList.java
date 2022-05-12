2da7368ca131940b2f8415d188362d1c63f3e97a
victorcuervo <vcuervo@gmail.com>
2022-05-12 10:37:01 +0200

    Añadida URL

--- src/com/lineadecodigo/java/util/collections/lista/AjustarCapacidadArrayList.java
+++ src/com/lineadecodigo/java/util/collections/lista/AjustarCapacidadArrayList.java
@@ -5,7 +5,7 @@ package com.lineadecodigo.java.util.collections.lista;
  * @version 1.0
  * @author Víctor Cuervo (http://lineadecodigo.com)
  * @date   16/agosto/2021
- * @url    http://lineadecodigo.com/java/xxx/
+ * @url    http://lineadecodigo.com/java/ajustar-la-capacidad-de-un-arraylist/
  * @description Se ajusta la capacidad del ArrayList
  */
 
@@ -15,13 +15,13 @@ public class AjustarCapacidadArrayList {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> al = new ArrayList<String>();
+		ArrayList<String> al = new ArrayList<String>(10);
 		
 		// Añadir elementos a un ArrayList
 		al.add("Victor");
 		al.add("Luis");
 		al.add("Marta");
-	
+
 		// Se ajusta el tamaño. Por defecto Java tiene preparado 1,5 la capacidad actual
 		al.trimToSize();
 		
